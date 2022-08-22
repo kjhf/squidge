9836deef023490eedcf271f308585525d2b54841
Slate <976725+kjhf@users.noreply.github.com>
2022-08-22 20:21:26 +0100

    get src from env file

--- src/squidge/entry/main.py
+++ src/squidge/entry/main.py
@@ -6,12 +6,17 @@ import logging
 
 
 def main():
-    sys.path.insert(0, '../../../src')
-    dotenv.load_dotenv()
+    dotenv_path = dotenv.find_dotenv()
+    if not dotenv_path:
+        assert False, ".env file not found. Please check the .env file is present in the root folder."
+    sys.path.insert(0, os.path.dirname(dotenv_path))
+
+    dotenv.load_dotenv(dotenv_path)
     if not os.getenv("DISCORD_BOT_TOKEN", None):
         assert False, "DISCORD_BOT_TOKEN is not defined, please check the .env file is present and correct."
 
     # Import must be after the env loading
+    print(sys.path)
     from src.squidge.entry.SquidgeBot import SquidgeBot
 
     logging.basicConfig(level=logging.INFO)
