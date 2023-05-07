488ee9d2da6dbece5880bd476cec8bb0b85e99d8
Slate <976725+kjhf@users.noreply.github.com>
2023-05-07 01:03:10 +0100

    Added topics

--- src/squidge/cogs/bot_util_commands.py
+++ src/squidge/cogs/bot_util_commands.py
@@ -1,10 +1,12 @@
 """Bot Utility commands cog."""
 import os
+import random
 
 from discord.ext import commands
 from discord.ext.commands import Context
 
 from src.squidge.entry.consts import COMMAND_SYMBOL
+from src.squidge.savedata.topics import TOPICS
 
 
 class BotUtilCommands(commands.Cog):
@@ -33,3 +35,14 @@ class BotUtilCommands(commands.Cog):
         pass_ctx=True)
     async def invite(self, ctx: Context):
         await ctx.send(f"https://discordapp.com/oauth2/authorize?client_id={os.getenv('DISCORD_BOT_CLIENT_ID')}&scope=bot%20applications.commands&permissions=2147483648")
+
+    @commands.command(
+        name='Random topic',
+        description="Gives you a random topic to discuss.",
+        brief="Gives you a random topic to discuss.",
+        aliases=['topic'],
+        help=f'{COMMAND_SYMBOL}topic',
+        pass_ctx=True)
+    async def topic(self, ctx: Context):
+        topic_to_discuss = random.choice(TOPICS)
+        await ctx.send(f"You should discuss... `{topic_to_discuss}` ...Go!")
